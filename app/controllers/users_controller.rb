0d7451e5054d12c6838da0646dddb2fd47e5f34d
Gopi Kumar <kumar91gopi@gmail.com>
2015-08-12 07:00:41 +0000

    finished login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,12 @@ class UsersController < ApplicationController
   end
   
   def create
-        @user = User.new(user_params)    
+        @user = User.new(user_params) 
+       
     if @user.save
+      log_in @user
       flash[:success] = "Welcome Aboard!"
-      redirect_to @user  # user_path(@user) is equivalent to @user 
+      redirect_to @user  #  user_path(@user)is equivalent to @user 
     else
       render 'new'
     end
@@ -21,7 +23,7 @@ class UsersController < ApplicationController
   private
   
       def user_params
-        params.require(:user).permit(:name, :email ,:password ,
+        params.require(:user).permit(:name, :email,:password,
                         :password_confirmation)
       end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   resources :users
   
   get 'users/new'
@@ -9,4 +11,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
